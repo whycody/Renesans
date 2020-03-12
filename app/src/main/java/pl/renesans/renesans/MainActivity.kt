58ac817dd5c424c3034eb0f1ff6e9b3a47885973
Oktawian Kausz <play26400@gmail.com>
2020-03-12 14:24:06 +0100

    Added Bottom Navigation Bar and Toolbar to Main Activity. Created fragment: DiscoverFragment, MapFragment and SettingsFragment

--- app/src/main/java/pl/renesans/renesans/MainActivity.kt
+++ app/src/main/java/pl/renesans/renesans/MainActivity.kt
@@ -2,11 +2,58 @@ package pl.renesans.renesans
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import pl.renesans.renesans.discover.DiscoverFragment
+import pl.renesans.renesans.map.MapFragment
+import pl.renesans.renesans.settings.SettingsFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private val discoverFragment = DiscoverFragment()
+    private val mapFragment = MapFragment()
+    private val settingsFragment = SettingsFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(mainToolbar as Toolbar)
+        mainNav.setOnNavigationItemSelectedListener(this)
+        changeFragment(discoverFragment, "discover")
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        return when(p0.itemId) {
+            R.id.discover -> {
+                changeFragment(discoverFragment, "discover")
+                true
+            }R.id.map -> {
+                changeFragment(mapFragment, "map")
+                true
+            }R.id.settings -> {
+                changeFragment(settingsFragment, "settings")
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment, tagFragmentName: String) {
+        val mFragmentManager = supportFragmentManager
+        val fragmentTransaction = mFragmentManager.beginTransaction()
+        val currentFragment = mFragmentManager.primaryNavigationFragment
+        if (currentFragment != null) fragmentTransaction.hide(currentFragment)
+        var fragmentTemp = mFragmentManager.findFragmentByTag(tagFragmentName)
+        if (fragmentTemp == null) {
+            fragmentTemp = fragment
+            fragmentTransaction.add(R.id.mainFrameLayout, fragmentTemp, tagFragmentName)
+        } else fragmentTransaction.show(fragmentTemp)
+        fragmentTransaction.setPrimaryNavigationFragment(fragmentTemp)
+        fragmentTransaction.setReorderingAllowed(true)
+        fragmentTransaction.commitNowAllowingStateLoss()
     }
 }
